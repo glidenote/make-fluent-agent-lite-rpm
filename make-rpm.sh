79d573ed78318fd5a047ef91572257c9f0d9d009
Akira Maeda <glidenote@gmail.com>
2013-12-13 10:32:58 +0900

    package名とダウンロードURL変更に対応

--- make-rpm.sh
+++ make-rpm.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ -z "$1" ]; then
-  echo "usage: ./make-rpm.sh 0.9"
+  echo "usage: ./make-rpm.sh 1.0"
   exit 1
 fi
 version=$1
@@ -9,7 +9,8 @@ cur=`pwd`
 
 rm -f fluent-agent-lite.v$version.tar.gz
 rm -fr fluent-agent-lite
-wget http://tagomoris.github.io/tarballs/fluent-agent-lite.v$version.tar.gz
+wget --no-check-certificate -O fluent-agent-lite.v$version.tar.gz \
+  https://github.com/tagomoris/fluent-agent-lite/archive/v$version.tar.gz
 tar xvzf fluent-agent-lite.v$version.tar.gz
 
 # setup rpmbuild env
@@ -20,13 +21,13 @@ mkdir rpmbuild
 pushd rpmbuild
 mkdir BUILD RPMS SOURCES SPECS SRPMS
 # locate spec
-cp ../fluent-agent-lite-v$version/SPECS/fluent-agent-lite.spec SPECS
+cp ../fluent-agent-lite-$version/SPECS/fluent-agent-lite.spec SPECS
 # locate source tarball
-mv ../fluent-agent-lite.v$version.tar.gz SOURCES
+mv ../fluent-agent-lite.v$version.tar.gz SOURCES/fluent-agent-lite.$version.tar.gz
 # locate conf
-cp ../fluent-agent-lite-v$version/package/fluent-agent-lite.conf SOURCES
+cp ../fluent-agent-lite-$version/package/fluent-agent-lite.conf SOURCES
 # locate init.d script
-cp ../fluent-agent-lite-v$version/package/fluent-agent-lite.init SOURCES
+cp ../fluent-agent-lite-$version/package/fluent-agent-lite.init SOURCES
 # build
 rpmbuild -v -ba --clean SPECS/fluent-agent-lite.spec
 popd
